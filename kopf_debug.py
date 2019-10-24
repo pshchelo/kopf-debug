cd6c622c5b776af5898adb06b21e7e9dacbdd350
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2019-10-24 13:42:36 +0000

    Set separate handler functions

--- kopf_debug.py
+++ kopf_debug.py
@@ -5,15 +5,29 @@ import time
 import kopf
 
 @kopf.on.create("zalando.org", "v1alpha1", "kopfdebugs")
-@kopf.on.update("zalando.org", "v1alpha1", "kopfdebugs")
-@kopf.on.resume("zalando.org", "v1alpha1", "kopfdebugs")
-async def apply(**kwargs):
-    fns = {s: functools.partial(dummy, s) for s in range(10)}
-    await kopf.execute(fns=fns)
-    return {"message": "applied"}
+async def create(body, logger, event, **kwargs):
+    # fns = {s: functools.partial(dummy, s) for s in range(10)}
+    # await kopf.execute(fns=fns)
+    await dummy(9, logger, event)
+    return {"message": "created"}
 
-def dummy(s, logger, event, **kwargs):
+@kopf.on.update("zalando.org", "v1alpha1", "kopfdebugs")
+async def update(*body, logger, event, **kwargs):
+    # fns = {s: functools.partial(dummy, s) for s in range(10)}
+    # await kopf.execute(fns=fns)
+    await dummy(9, logger, event)
+    return {"message": "updated"}
+
+@kopf.on.resume("zalando.org", "v1alpha1", "kopfdebugs")
+async def resume(body, logger, event, **kwargs):
+    # fns = {s: functools.partial(dummy, s) for s in range(10)}
+    # await kopf.execute(fns=fns)
+    import pdb; pdb.set_trace()  # XXX BREAKPOINT
+    await dummy(9, logger, event)
+    return {"message": "resumed"}
+
+async def dummy(s, logger, event, **kwargs):
     logger.info(f"Got event {event}")
-    # await asyncio.sleep(10-s)
-    time.sleep(10-s)
+    await asyncio.sleep(10-s)
+    # time.sleep(10-s)
     logger.info(f"slept for {10 - s}")
