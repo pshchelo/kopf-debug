893ff6312251c51cb8a39b9e3a62c2e10389989c
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2019-11-11 17:14:59 +0000

    Add minimal example for resume testing

---
+++ resume.py
@@ -0,0 +1,13 @@
+import kopf
+
+
+@kopf.on.update("zalando.org", "v1", "kopfexamples")
+@kopf.on.create("zalando.org", "v1", "kopfexamples")
+async def handle_apply(**kwargs):
+    ...
+
+
+@kopf.on.resume("zalando.org", "v1", "kopfexamples")
+async def handle_resume(logger, **kwargs):
+    for k, v in kwargs.items():
+        logger.info(f"kwarg {k} = {v}")
