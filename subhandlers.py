eb1f5e565b799493ef1973054e8866dacb55faa9
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2020-01-14 18:17:44 +0000

    Update for minimal repro for issue279

--- subhandlers.py
+++ subhandlers.py
@@ -8,9 +8,13 @@ N_HANDLERS = 5
 @kopf.on.create("zalando.org", "v1", "kopfexamples")
 async def ensure(body, logger, event, **kwargs):
     fns = {s: functools.partial(handle, s) for s in range(N_HANDLERS)}
-    await kopf.execute(fns=fns)
+    for n, f in fns.items():
+        await f(logger, event, body, **kwargs)
+    # await kopf.execute(fns=fns)
     return {"message": f"{event}d"}
 
 
-async def handle(s, logger, event, spec, **kwargs):
+async def handle(s, logger, event, body, spec, **kwargs):
+    if s == 1 and kwargs['retry'] == 0:
+        raise kopf.TemporaryError(f"Handler {s} failed, will retry")
     logger.info(f"Handler {s} for event {event} with field {spec['field']}")
